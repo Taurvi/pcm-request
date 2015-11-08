f924d18bee77a3c6734295d134f7216d1191db5c
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2015-11-07 20:06:27 -0800

    Setup core CSS and JS imports

---
+++ node/index.js
@@ -0,0 +1,27 @@
+serverMsg = function(msg) {
+    console.log('|| SERVER || ' + msg);
+};
+
+serverMsg('node.js is initializing');
+
+// Initialization
+var app = require('express')();
+serverMsg(' Loaded express.');
+var http = require('http').Server(app);
+serverMsg(' Loaded http.');
+var io = require('socket.io')(http);
+serverMsg(' Loaded socket.io.');
+var crypto = require('crypto');
+serverMsg(' Loaded crypto.');
+var Twilio = require('twilio')('ACCOUNT_SID', 'AUTH_TOKEN');
+serverMsg(' Loaded Twilio.');
+var Trello = require("node-trello");
+serverMsg(' Loaded Trello.');
+var t = new Trello("<your key>", "<token>");
+
+serverMsg('Server has initialized!');
+
+// User connects to socketIO
+io.sockets.on('connection', function(socket){
+
+});
